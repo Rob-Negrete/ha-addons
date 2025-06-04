1cdc36efad9ff1bcdab0f17fd108fa358e37fb8e
Rob Negrete <rsnegrete@gmail.com>
2025-06-04 08:32:23 -0600

    Update run.sh to start addon

--- face-rekon/run.sh
+++ face-rekon/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo "Iniciando Face Rekon..."
+mkdir -p /config/face-rekon/db
+mkdir -p /config/face-rekon/images
+mkdir -p /config/face-rekon/faces
+mkdir -p /config/face-rekon/unknowns
+
 python3 /app/scripts/clasificador.py
+
+# Mantener el contenedor activo para diagnóstico (puedes quitarlo si lo deseas)
 tail -f /dev/null
